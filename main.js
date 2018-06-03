d3cb41dd8ae624592abbc891796be4e7dad68ab2
pedro-rodalia <pedro.rodalia@gmail.com>
2018-06-03 17:12:22 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require( 'express' )
+const app = express()
+const server = require( 'http' )
+	.Server( app )
+const io = require( 'socket.io' )( server )
+
+// Sockets
+io.on( 'connection', ( socket ) => {
+	console.log( `User connected with id: '${socket.id}'` )
+} )
+
+// Server
+server.listen( 8080, () => {
+	console.log( 'Server started on port: 8080' )
+} )
